5dcbb98198fe70590bda780fa5f616a65b02dee9
miguel miguel <miguef10hd@gmail.com>
2023-02-09 18:50:48 -0500

    Version 2

--- SerieFourierOndaTriangular.m
+++ SerieFourierOndaTriangular.m
@@ -3,7 +3,10 @@
 function[]=mfuncion()
 t=-pi:0.01:pi;
 f=(t<=0).*(0)+(t>0).*(pi-t);
-plot(t,f);
+p=plot(t,f,'-*', 'MarkerIndices',1:30:length(f));
+p(1).LineWidth = 2;
+title('Fourier Serie')
+legend({'Function to parts',},'Location','northwestoutside')
 hold on
 
 %Input of variables a0, an, bn
@@ -14,13 +17,15 @@ T=2*pi;
 a0=pi/2;
 
 %Loop for different values of n
-for n=1:6
+for n=1:6%---------------------Edit this variable for different value of n
     an=an+((1-((-1)^n))/((n^2)*pi))*cos((2*n*pi*t)/T);
     bn=bn+(1/n)*sin((2*n*pi*t)/T);
     ft=(a0/2)+an+bn;
-    plot(t,ft)
+    plot(t,ft,'DisplayName',sprintf('n = %d',n))
 end
+legend()
 hold off
+grid on
 
 end
 
