32da955b76d5c3b118560e2a490feac8f455f0fb
miguel miguel <miguef10hd@gmail.com>
2023-02-09 18:13:17 -0500

    Add title

--- SerieFourierOndaTriangular.m
+++ SerieFourierOndaTriangular.m
@@ -1,53 +1,28 @@
-
-% t=linspace(-pi,pi);
-% y=pi-t;
-% plot(t,y)
-% title('Fourier')
+%Graph of the Fourier Serie of example classic of triangle wave
+%Graph of the function by parts
 function[]=mfuncion()
 t=-pi:0.01:pi;
 f=(t<=0).*(0)+(t>0).*(pi-t);
 plot(t,f);
-
 hold on
+
+%Input of variables a0, an, bn
 t=linspace(-pi,pi);
-% n1=1;
 an=0;
 bn=0;
 T=2*pi;
 a0=pi/2;
 
-for n=1:8
-    
+%Loop for different values of n
+for n=1:6
     an=an+((1-((-1)^n))/((n^2)*pi))*cos((2*n*pi*t)/T);
     bn=bn+(1/n)*sin((2*n*pi*t)/T);
-%     ft=a0+an+bn;
-%     plot(t,ft)
+    ft=(a0/2)+an+bn;
+    plot(t,ft)
 end
-
-% an=((1-((-1)^n))/((n^2)*pi))*cos(n*t);
-% bn=(1/n)*sin(n*t);
-   
-ft=(a0/2)+an+bn;
-plot(t,ft)
-
 hold off
+
 end
-
-
-% f=0;
-% f1=0;
-% 
-% 
-% for n=1:1
-%     f=f+(n*2)
-%     f1=f1+(n*3)
-% end
-% ft=f+f1
-
-
-
-
-
 
 
 
